4012c09d5fb90a76313bec4cfc56879e523a7738
Sharma Amit <T0250221@gemalto.com>
2021-11-19 02:08:59 +0530

    Bootstrapped symptoms a bit

--- src/components/pages/Precautions/Precautions.js
+++ src/components/pages/Precautions/Precautions.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Precautions.css';
-
+import useWindowDimensions from '../../UseDimensions';
 function Precautions() {
+
+    const { height, width } = useWindowDimensions();
+
     return (
         <div className='pricing__section'>
             <div className='pricing__wrapper'>
@@ -13,36 +16,46 @@ function Precautions() {
                 </div>
                 <div className='precautions__container'>
                     <div className="precautions-row">
-                        <img src="images/pr1.svg" style={{ maxWidth: '60px', marginTop: '-11%' }} />
-                        <div style={{ maxWidth: '30%', padding: '0px 50px' }}>
-                            <h3>Wear Masks</h3>
-                            <p>Continually seize impactful vortals rather than future-proof supply chains. Uniquely exploit emerging niches via fully tested meta-services. Competently pursue standards compliant leadership skills vis-a-vis pandemic "outside the box" thinking. Objectively </p>
+                        <div className="precaution-info">
+                            <img src="images/pr1.svg" style={{ maxWidth: '60px', maxHeight: '60px' }} />
+                            <div className="precautions-text">
+                                <h3>Wear Masks</h3>
+                                <p>Continually seize impactful vortals rather than future-proof supply chains. Uniquely exploit emerging niches via fully tested meta-services. Competently pursue standards compliant leadership skills vis-a-vis pandemic "outside the box" thinking. Objectively </p>
+                            </div>
                         </div>
                         <img src="images/pr1image.png" style={{ maxWidth: '30%' }}></img>
                     </div>
                     <div className="precautions-row">
-                        <img src="images/pr2image.png" style={{ maxWidth: '30%', marginRight: 100 }}></img>
-                        <img src="images/pr2.png" style={{ maxWidth: '60px', marginTop: '-11%' }} />
-                        <div style={{ maxWidth: '30%', padding: '0px 50px' }}>
-                            <h3>Wash Your Hands</h3>
-                            <p>Continually seize impactful vortals rather than future-proof supply chains. Uniquely exploit emerging niches via fully tested meta-services. Competently pursue standards compliant leadership skills vis-a-vis pandemic "outside the box" thinking. Objectively </p>
+                        {Number(width) > 768 ? <img src="images/pr2image.png" style={{ maxWidth: '30%' }}></img> : null}
+                        <div className="precaution-info">
+                            <img src="images/pr2.png" style={{ maxWidth: '60px', maxHeight: '60px' }} />
+                            <div className="precautions-text">
+                                <h3>Wash Your Hands</h3>
+                                <p>Continually seize impactful vortals rather than future-proof supply chains. Uniquely exploit emerging niches via fully tested meta-services. Competently pursue standards compliant leadership skills vis-a-vis pandemic "outside the box" thinking. Objectively </p>
+                            </div>
                         </div>
+                        {Number(width) < 768 ? <img src="images/pr2image.png" style={{ maxWidth: '50%' }} /> : null}
                     </div>
                     <div className="precautions-row">
-                        <img src="images/pr3.png" style={{ maxWidth: '60px', marginTop: '-11%' }} />
-                        <div style={{ maxWidth: '30%', padding: '0px 50px' }}>
-                            <h3>Use Nose-Rag</h3>
-                            <p>Continually seize impactful vortals rather than future-proof supply chains. Uniquely exploit emerging niches via fully tested meta-services. Competently pursue standards compliant leadership skills vis-a-vis pandemic "outside the box" thinking. Objectively </p>
+                        <div className="precaution-info">
+                            <img src="images/pr3.png" style={{ maxWidth: '60px', maxHeight: '60px' }} />
+                            <div className="precautions-text">
+                                <h3>Use Nose-Rag</h3>
+                                <p>Continually seize impactful vortals rather than future-proof supply chains. Uniquely exploit emerging niches via fully tested meta-services. Competently pursue standards compliant leadership skills vis-a-vis pandemic "outside the box" thinking. Objectively </p>
+                            </div>
                         </div>
-                        <img src="images/pr3image.png" style={{ maxWidth: '30%' }}></img>
+                        <img src="images/pr3image.png" style={{ Width: '100%', maxHeight: '50%' }}></img>
                     </div>
                     <div className="precautions-row">
-                        <img src="images/pr4image.png" style={{ maxWidth: '30%', marginRight: 100 }}></img>
-                        <img src="images/pr1.svg" style={{ maxWidth: '60px', marginTop: '-11%' }} />
-                        <div style={{ maxWidth: '30%', padding: '0px 50px' }}>
-                            <h3>Avoid Contacts</h3>
-                            <p>Continually seize impactful vortals rather than future-proof supply chains. Uniquely exploit emerging niches via fully tested meta-services. Competently pursue standards compliant leadership skills vis-a-vis pandemic "outside the box" thinking. Objectively </p>
+                        {Number(width) >768 ? <img src="images/pr4image.png" style={{ maxWidth: '30%', marginRight: 100 }}></img> : null }
+                        <div className="precaution-info">
+                            <img src="images/pr4.png" style={{ maxWidth: '60px', maxHeight: '60px' }} />
+                            <div className="precautions-text">
+                                <h3>Avoid Contacts</h3>
+                                <p>Continually seize impactful vortals rather than future-proof supply chains. Uniquely exploit emerging niches via fully tested meta-services. Competently pursue standards compliant leadership skills vis-a-vis pandemic "outside the box" thinking. Objectively </p>
+                            </div>
                         </div>
+                        {Number(width) < 768 ? <img src="images/pr4image.png" style={{ maxWidth: '100%' }} /> : null}
                     </div>
                 </div>
             </div>
